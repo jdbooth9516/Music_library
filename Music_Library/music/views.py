95d2ccc614cd58d452b003413d85895c5ff1c72a
Jeremiah Booth <jeremiahbooth@pop-os.localdomain>
2021-06-11 12:01:19 -0700

    added the delete functionally and pathing

--- Music_Library/music/views.py
+++ Music_Library/music/views.py
@@ -8,6 +8,8 @@ from rest_framework.response import Response
 from rest_framework import status
 
 
+
+
 class SongList(APIView):
 
     def get(self, request):
@@ -38,6 +40,21 @@ class SongDetails(APIView):
     def get(self, request, pk):
         song = self.get_song(pk)
         serializer = SongSerializer(song)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        song = self.get_song(pk)
+        serializer = SongSerializer(song, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        song = self.get_song(pk)
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
        
         
